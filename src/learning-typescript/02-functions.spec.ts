a1352dfcb0a7fe1bd3c0c902defcc6559caed390
Ikromer-SE <73547137+Ikromer-SE@users.noreply.github.com>
2021-09-21 14:56:58 -0400

    Add more tests

--- src/learning-typescript/02-functions.spec.ts
+++ src/learning-typescript/02-functions.spec.ts
@@ -1,3 +1,5 @@
+import { isEven, tagMaker } from "src/app/utils";
+
 describe('some function stuff', () => {
 
   describe('higher order functions', () => {
@@ -34,16 +36,39 @@ describe('some function stuff', () => {
     });
 
     it('higher order function', () => {
-      const tagMaker = (tag: string) => (content: string) => `<${tag}>${content}</${tag}>`;
-
       const h1Maker = tagMaker('h1'); // "closures"
       const pMaker = tagMaker('p');
-
 
       expect(h1Maker('Hello')).toBe('<h1>Hello</h1>');
       expect(h1Maker('Goodbye')).toBe('<h1>Goodbye</h1>');
       expect(h1Maker('Tacos!')).toBe('<h1>Tacos!</h1>');
       expect(pMaker('Message')).toBe('<p>Message</p>');
+    });
+
+    it('some details on parameters to functions', () => {
+
+      function formatName(first: string, last: string, mi?: string): string {
+        if (mi) {
+          return `${last}, ${first} ${mi}.`;
+        } else {
+          return `${last}, ${first}`;
+        }
+      }
+
+      expect(formatName('Han', 'Solo')).toBe('Solo, Han');
+      expect(formatName('Han', 'Solo', 'D')).toBe('Solo, Han D.');
+    });
+
+    it('truth table', () => {
+      expect(!!"tacos").toBe(true);
+      expect("tacos").toBeTruthy();
+      expect("").toBeFalsy();
+      expect(undefined).toBeFalsy();
+      expect(null).toBeFalsy();
+      expect(0).toBeFalsy();
+      expect(1).toBeTruthy();
+      expect(-1).toBeTruthy();
+
     });
   });
 });
