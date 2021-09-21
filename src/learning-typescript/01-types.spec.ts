79dde125543bc9fd7ef779125ce95cf6e00910b5
Ikromer-SE <73547137+Ikromer-SE@users.noreply.github.com>
2021-09-21 12:21:57 -0400

    Add more tests

--- src/learning-typescript/01-types.spec.ts
+++ src/learning-typescript/01-types.spec.ts
@@ -13,7 +13,6 @@ describe('Data types and variables and constants in Typescript', () => {
       // y = 44; is invalid since y is implicity a string
 
       expect(typeof (y)).toBe('string')
-
     });
 
     it('should be explicity typed variables', () => {
@@ -51,7 +50,6 @@ describe('Data types and variables and constants in Typescript', () => {
         actors: ['Zendaya', 'Timothy Chalamet']
       });
     });
-
   });
 
   describe('Typescript literals', () => {
@@ -91,8 +89,6 @@ describe('Data types and variables and constants in Typescript', () => {
       let n6: number = 180_000;
 
       expect(n6).toBe(180000);
-
-
     });
 
     it('has array literals', () => {
@@ -106,10 +102,122 @@ describe('Data types and variables and constants in Typescript', () => {
       let bools: boolean[];
 
       // let fn: (string | number)[];
+    });
+
+    it('should be object literals', () => {
+
+      interface Book {
+        title: string,
+        author: string,
+        yearReleased: number,
+        publisher?: string
+      }
+
+      const book: Book = {
+        title: 'Dune',
+        author: 'Frank Herbert',
+        yearReleased: 1963
+      }
+
+      const book2: Book = {
+        title: 'Dune 2',
+        author: 'Frank Herbert',
+        yearReleased: 1963,
+        publisher: 'Book Company'
+      }
+
+      expect(book.title).toEqual('Dune');
+      expect(book2.title).toEqual('Dune 2');
+
+      interface Actor {
+        firstName: string;
+        lastName: string;
+        roles: {
+          part: string;
+          films: string[];
+        }[];
+      }
+      const actor: Actor = {
+        firstName: 'Mark',
+        lastName: 'Hammil',
+        roles: [
+          { part: 'Luke Skywalker', films: ['Star Wars', 'The Empire Strikes Back'] }
+        ]
+      }
+
+      const actor2: Actor = {
+        firstName: 'Kyle',
+        lastName: 'McCloughlin',
+        roles: [
+          { part: 'Special Agent Dale Cooper', films: ['Twin Peaks', 'Twin Peaks Firewalk With Me', 'Twin Peaks The Return'] }
+        ]
+      };
+
+      expect(actor.firstName).toEqual('Mark');
+      expect(actor2.firstName).toEqual('Kyle');
+    });
+
+    it('duck typing', () => {
+
+      interface Loggable { message: string, for: string }
+
+      // "If it walks like a duck and talks like a duck its probably a duck" - "Structural Typing"
+      function log(loggable: Loggable) {
+        console.log(`Logging ${loggable.message} for  ${loggable.for} at ${new Date().toISOString()} `)
+      }
+
+      const phoneCall = {
+        for: 'Bob',
+        message: 'Your launday is ready',
+        from: 'Joe\'s laundry',
+        number: '555-1212'
+      }
+
+      log(phoneCall)
+    });
+
+    it('function literals', () => {
+
+      // Can do this before declaration since it is not anonymous
+      expect(add(2, 2)).toBe(4);
+
+      // Named Function
+      function add(a: number, b: number): number {
+        return a + b;
+      }
+
+      // Add a type to use in anonymous functions
+      type MathOp = (a: number, b: number) => number;
+
+      const someMathOp: MathOp = (a, b) => a + b;
+
+      // Anonymous function
+      // probably don't do this. Old Skool, you look like a n00b
+      const subtract = function (a: number, b: number): number {
+        return a - b;
+      }
+
+      // Anonymous function with an arrow function
+      // This is the same as int multiply(int a, int b) {return a*b};
+      const multiply = (a: number, b: number): number => a * b;
+
+      //This needs to be after declaration
+      expect(multiply(3, 3)).toBe(9);
+
+      const divide = (a: number, b: number): number => {
+        if (b === 0) { throw new Error("Are you trying to open a black hole!?") };
+        return a / b;
+      }
+
+      function doubleThemAndDoMath(x: number, y: number, f: MathOp) {
+        return f(x + x, y + y);
+      }
+
+      expect(doubleThemAndDoMath(2, 2, add)).toBe(8);
+      expect(doubleThemAndDoMath(3, 3, multiply)).toBe(36);
+      expect(doubleThemAndDoMath(2, 2, (x, y) => x / y)).toBe(1);
 
     });
 
-    it('function literals');
-    it('object literals');
   });
 });
