ccadee6bf316f5b56af97b87f9f5fb87ae695021
Ikromer-SE <73547137+Ikromer-SE@users.noreply.github.com>
2021-09-22 12:07:01 -0400

    Add UI and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { AboutComponent } from './components/about/about.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
+import { TodoEntryComponent } from './components/todos/todo-entry/todo-entry.component';
+import { TodoListComponent } from './components/todos/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    NavigationComponent,
+    DashboardComponent,
+    TodosComponent,
+    AboutComponent,
+    PrivacyComponent,
+    TodoEntryComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
